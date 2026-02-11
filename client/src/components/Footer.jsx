917a8bb2bb2497a6e5f6568d1f8dea5f2ee63e5c
syedibrahim8 <syedibrahimofficial1@gmail.com>
2026-02-12 00:13:34 +0300

    worked

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer(){
     return(
         <>
             <footer className="py-4 text-center bg-black text-white text-sm z-10">
-            Syed Ibrahim Ali © 2026 Teach Parents Smartphone
+            Syed Ibrahim Ali © Copyrighs 2026 Teach Parents Smartphone
             </footer>
         </>
     )
